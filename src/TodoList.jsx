e355ed7526b4fb204788d549350d2beab4091269
MariaZasypkina <maria.zasypkina@gmail.com>
2024-11-14 18:08:07 -0700

    Added functionality to input new tasks into the to-do list, refactored list items into a separate component, and managed task state updates in App

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,24 +1,25 @@
+import TodoListItem from "./TodoListItem";
 
 const todoList = []; //creating an empty array and srore it in the variable
 
-todoList.push( //inside the array creating 3 objects with properties id and title
+todoList.push( //inside the array creating 4 objects with properties id and title
   {id: 1, title: "Review assignment"},
   {id: 2, title: "Complete assignment"},
   {id: 3, title: "Create mindset resposes"},
   {id:4, title: "Submit assignment"}
 );
 
-
+//
 
 function TodoList(){
+
     return (
         <ul style={{ textAlign: 'left'}}> 
-        {todoList.map((item) => ( 
-            <li key={item.id}>{item.title}</li>
-          ))
-          }
+        {todoList.map(todo => ( 
+            <TodoListItem key={todo.id} item={todo} />
+          ))}
           </ul>
     );
-};
+}
 
 export default TodoList;
---
+++ src/TodoListItem.jsx
@@ -0,0 +1,10 @@
+
+function TodoListItem({ item }){
+
+return (
+
+    <li>{item.title}</li>
+);
+}
+
+export default TodoListItem;
